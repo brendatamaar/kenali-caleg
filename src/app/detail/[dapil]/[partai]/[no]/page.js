ecacfe1af0d3528f00bab9ad713721340bcd2cd7
brenda <brendatama.ramadhan@iconpln.co.id>
2024-02-10 22:25:02 +0700

    bugfix

--- src/app/detail/[dapil]/[partai]/[no]/page.js
+++ src/app/detail/[dapil]/[partai]/[no]/page.js
@@ -33,7 +33,7 @@ function Page({ params }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-gray-50">
       {calegs.map((caleg, idx) => (
-        <div className="max-w-lg mx-4 my-10 w-full">
+        <div className="max-w-lg mx-4 my-10 w-full" key={idx}>
           <button
             onClick={() => router.push(`/`)}
             className="text-white w-32 mt-4 bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
